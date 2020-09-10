e17a786e9f559b713a57027ee6563641c9861ef0
Carson <carsonhake@gmail.com>
2020-09-09 21:42:29 -0400

    New Callbacks, added Pytorch Hook Functionality, new Modules
    -All models now run on GPU
    -Added SuperPixels
    	a) superpixels is based on a 2010 paper
    	b)main idea: cluster pixels with similar characteristics to
    	perform image segmentation
    	c)interesting result given simplicity
    -Added Callbacks:
    	1)CudaCallback - puts model and dataloaders onto gpu
    	2)IndependerntVarBatchTranformCallback - gives ability to
    	perform transformation on inputs
    -New nn.Module: SequentialModel
    	-based on nn.Sequential layers
    	-stores layers in an nn.ModuleList
    	-able to be called with an input
    	-can iterate through layers
    -Pytorch Hooks
    	1)ForwardHooks Class - used to initialize and store information
    	used in the Pytorch Forward Hook
    	2)Hooks - a class that stores n-hooks
    		a)acts close to a numpy array
    		b)iterative
    		c)"with" block functionality
    		d)automatic deletion and item removal from memory
    -helper functions added to utilities.imports module

--- utilties/imports.py
+++ utilties/imports.py
@@ -3,11 +3,12 @@ from pathlib import Path
 from IPython.core.debugger import set_trace
 import pickle, gzip, math, torch, re, matplotlib as mpl, matplotlib.pyplot as plt
 from functools import partial
-
+import numpy
 from typing import Iterable
 from torch import tensor, nn, optim
 from torch.utils.data import DataLoader, TensorDataset,  SequentialSampler, RandomSampler
 import torch.nn.functional as F
+import torch.nn.init
 
 #helper functions
 _camel_re1 = re.compile('(.)([A-Z][a-z]+)')
@@ -25,6 +26,40 @@ def convert_to_list(o):
 
 def accuracy(out, yb): #
      return (torch.argmax(out, dim=1)==yb).float().mean()
+
+def normalize(x, m, s):
+    return (x-m)/s
+
+#normalize datasets
+def normalize_to(train, valid):
+    m,s = train.mean(),train.std()
+    return normalize(train, m, s), normalize(valid, m, s)
+
+#resize mnist image data -> 28x28
+def mnist_resize(x): return x.view(-1, 1, 28, 28)
+
+#flatten an image to 
+def flatten(x):
+    return x.view(x.shape[0], -1) #removes 1,1 axis from result of AvgPool layer
+
+#re-view an x variable at a specific size
+def view_tfm(*size):
+    def _inner(x): return x.view(*((-1,)+size))
+    return _inner
+
+#creating 2d convolution models
+def conv2d(ni, nf, ks=3, stride=2):
+    return nn.Sequential(
+        nn.Conv2d(ni, nf, ks, padding=ks//2, stride=stride), nn.ReLU())
+
+def get_cnn_layers(num_categories, nfs):
+    nfs = [1] + nfs
+    return [
+        conv2d(nfs[i], nfs[i+1], 5 if i==0 else 3) for i in range(len(nfs)-1)
+    ] + [nn.AdaptiveAvgPool2d(1), Lambda(flatten), nn.Linear(nfs[-1], num_categories)]
+
+def get_cnn_model(num_categories, nfs):
+    return nn.Sequential(*get_cnn_layers(num_categories, nfs))
 
 #returns the model and optimizer, will be refactored to be more versatile, currently just used for simple testing
 def get_model(training_dl, lr=0.5, nh=50):
@@ -50,37 +85,6 @@ class Dataset():
     
     def __getitem__(self, i):
         return self.x_dataset[i],self.y_dataset[i]
-
-
-#used to implement extra functionallity into our training
-class Callback():
-    _order=0
-    def set_runner(self, runner): self.run=runner
-    def __getattr__(self, k): return getattr(self.run, k)
-    @property
-    def name(self):
-        name = re.sub(r'Callback$', '', self.__class__.__name__)
-        return camel2snake(name or 'callback')
-
-#callback included in every runner, tracks iterations and epochs
-class TrainEvalCallback(Callback):
-    def begin_fit(self):
-        self.run.n_epochs=0.
-        self.run.n_iter=0
-    
-    def after_batch(self):
-        if not self.in_train: return
-        self.run.n_epochs += 1./self.iters
-        self.run.n_iter   += 1
-        
-    def begin_epoch(self):
-        self.run.n_epochs=self.epoch
-        self.model.train()
-        self.run.in_train=True
-
-    def begin_validate(self):
-        self.model.eval()
-        self.run.in_train=False
 
 class AvgStats():
     def __init__(self, metrics, in_train):
@@ -110,92 +114,186 @@ class AvgStats():
         for i,m in enumerate(self.metrics):
             self.total_metrics[i] += m(run.pred, run.yb) * batch_size
 
+
+
+class CancelTrainException(Exception): pass
+class CancelEpochException(Exception): pass
+class CancelBatchException(Exception): pass
+
+class Callback():
+    _order = 0
+    def set_runner(self, run): self.run=run
+    def __getattr__(self, k): return getattr(self.run, k)
+
+    @property
+    def name(self):
+        name = re.sub(r'Callback$', '', self.__class__.__name__)
+        return camel2snake(name or 'callback')
+
+    def __call__(self, cb_name):
+        f = getattr(self, cb_name, None)
+        if f and f(): return True
+        return False
+
+#our standard used callback, included in every model
+class TrainEvalCallback(Callback):
+    def begin_fit(self):
+        self.run.n_epochs=0.
+        self.run.n_iter=0
+
+    def after_batch(self):
+        if not self.in_train: return
+        self.run.n_epochs += 1./self.iters
+        self.run.n_iter   += 1
+        
+    def begin_epoch(self):
+        self.run.n_epochs=self.epoch
+        self.model.train()
+        self.run.in_train=True
+
+    def begin_validate(self):
+        self.model.eval()
+        self.run.in_train=False
+
 class AvgStatsCallback(Callback):
     def __init__(self, metrics):
-        self.train_stats = AvgStats(metrics,True)
-        self.valid_stats = AvgStats(metrics,False)
+        self.train_stats,self.valid_stats = AvgStats(metrics,True),AvgStats(metrics,False)
         
-    def begin_epoch(self):        
+    def begin_epoch(self):
         self.train_stats.reset()
         self.valid_stats.reset()
         
     def after_loss(self):
         stats = self.train_stats if self.in_train else self.valid_stats
-        with torch.no_grad():
-            stats.accumulate(self.run)
+        with torch.no_grad(): stats.accumulate(self.run)
     
     def after_epoch(self):
         print(self.train_stats)
         print(self.valid_stats)
+        
+class Recorder(Callback):
+    def begin_fit(self):
+        self.lrs = [[] for _ in self.opt.param_groups]
+        self.losses = []
 
-#used to define a model and train it
+    def after_batch(self):
+        if not self.in_train: return
+        for pg,lr in zip(self.opt.param_groups,self.lrs): lr.append(pg['lr'])
+        self.losses.append(self.loss.detach().cpu())        
+
+    def plot_lr  (self, pgid=-1): plt.plot(self.lrs[pgid])
+    def plot_loss(self, skip_last=0): plt.plot(self.losses[:len(self.losses)-skip_last])
+        
+    def plot(self, skip_last=0, pgid=-1):
+        losses = [o.item() for o in self.losses]
+        lrs    = self.lrs[pgid]
+        n = len(losses)-skip_last
+        plt.xscale('log')
+        plt.plot(lrs[:n], losses[:n])
+
+class ParamScheduler(Callback):
+    _order=1
+    def __init__(self, pname, sched_funcs): self.pname,self.sched_funcs = pname,sched_funcs
+        
+    def begin_fit(self):
+        if not isinstance(self.sched_funcs, (list,tuple)):
+            self.sched_funcs = [self.sched_funcs] * len(self.opt.param_groups)
+
+    def set_param(self):
+        assert len(self.opt.param_groups)==len(self.sched_funcs)
+        for pg,f in zip(self.opt.param_groups,self.sched_funcs):
+            pg[self.pname] = f(self.n_epochs/self.epochs)
+            
+    def begin_batch(self): 
+        if self.in_train: self.set_param()
+
+class CudaCallbackDev(Callback):
+    def __init__(self,device): self.device=device
+    def begin_fit(self): 
+        self.model.to(self.device)
+    def begin_batch(self):
+        self.run.xb,self.run.yb = self.xb.to(self.device),self.yb.to(self.device)
+
+class CudaCallback(Callback):
+    def begin_fit(self): self.model.cuda()
+    def begin_batch(self): self.run.xb,self.run.yb = self.xb.cuda(),self.yb.cuda()
+
+#applies a given transform to the independent variables (xs)
+class IndependentVarBatchTransformCallback(Callback):
+    _order=2
+    def __init__(self, tfm): self.tfm = tfm
+    def begin_batch(self): self.run.xb = self.tfm(self.xb)
+            
+
+
 class Runner():
-    def __init__(self, cbs=None, cb_functions=None):
-        callbacks = convert_to_list(cbs)
-        for cbf in convert_to_list(cb_functions): #convert functions to callbacks
+    def __init__(self, cbs=None, cb_funcs=None):
+        cbs = convert_to_list(cbs)
+        for cbf in convert_to_list(cb_funcs):
             cb = cbf()
             setattr(self, cb.name, cb)
-            callbacks.append(cb)
-        self.stop = False
-        self.callbacks = [TrainEvalCallback()]+callbacks
+            cbs.append(cb)
+        self.stop,self.cbs = False,[TrainEvalCallback()]+cbs
+
 
     def one_batch(self, xb, yb):
-        self.xb,self.yb = xb,yb
-        if self('begin_batch'): return
-        self.pred = self.model(self.xb)
-        if self('after_pred'): return
-        self.loss = self.loss_func(self.pred, self.yb)
-        if self('after_loss') or not self.in_train: return
-        self.loss.backward()
-        if self('after_backward'): return
-        self.opt.step()
-        if self('after_step'): return
-        self.opt.zero_grad()
+        try:
+            self.xb,self.yb = xb,yb
+            self('begin_batch')
+            self.pred = self.model(self.xb)
+            self('after_pred')
+            self.loss = self.loss_function(self.pred, self.yb)
+            self('after_loss')
+            if not self.in_train: return
+            self.loss.backward()
+            self('after_backward')
+            self.opt.step()
+            self('after_step')
+            self.opt.zero_grad()
+        except CancelBatchException: self('after_cancel_batch')
+        finally: self('after_batch')
 
     def all_batches(self, dl):
         self.iters = len(dl)
-        for xb,yb in dl:
-            
-            if self.stop: break
-            self.one_batch(xb, yb)
-            self('after_batch')
-        self.stop=False
-
-    #the new training loop
-    def fit(self, epochs_in, model_in, optimizer_in, loss_function_in, train_dl_in, valid_dl_in):
-        self.epochs = epochs_in
-        self.model = model_in
-        self.opt = optimizer_in
-        self.loss_func = loss_function_in
-        self.train_dl = train_dl_in
-        self.valid_dl = valid_dl_in
         try:
-            for cb in self.callbacks: cb.set_runner(self)
-            if self('begin_fit'): return
-            for epoch in range(self.epochs):
+            for xb,yb in dl: self.one_batch(xb, yb)
+        except CancelEpochException: self('after_cancel_epoch')
+
+    def fit(self, epochs, mod, optimizer, loss_func, train_dl, valid_dl):
+        self.epochs = epochs
+        self.loss = tensor(0.)
+        self.model = mod
+        self.opt = optimizer
+        self.loss_function = loss_func
+        self.valid_dl = valid_dl
+        self.train_dl = train_dl
+        try:
+            for cb in self.cbs: cb.set_runner(self)
+            self('begin_fit')
+            for epoch in range(epochs):
                 self.epoch = epoch
-                #training
-                if not self('begin_epoch'): #callbacks default to false returns
-                    self.all_batches(self.train_dl)
-                
-                #validation 
+                if not self('begin_epoch'): self.all_batches(self.train_dl)
+
                 with torch.no_grad(): 
-                    if not self('begin_validate'): 
-                        self.all_batches(self.valid_dl)
-                if self('after_epoch'): break
+                    if not self('begin_validate'): self.all_batches(self.valid_dl)
+                self('after_epoch')
             
+        except CancelTrainException: self('after_cancel_train')
         finally:
             self('after_fit')
-            self.model = None
-            self.opt = None
-            self.loss_func = None
-            self.train_dl = None
-            self.valid_dl = None
+            self.learn = None
 
     def __call__(self, cb_name):
-        for cb in sorted(self.callbacks, key=lambda x: x._order):
-            f = getattr(cb, cb_name, None)
-            if f and f():
-                return True
-        return False
-    
+        return_val = False
+        for cb in sorted(self.cbs, key=lambda x: x._order): return_val = cb(cb_name) or return_val
+        return return_val
+
+
+
+#Pytorch Module
+class Lambda(nn.Module):
+    def __init__(self, func):
+        super().__init__()
+        self.func = func
+
+    def forward(self, x): return self.func(x)
